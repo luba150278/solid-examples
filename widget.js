485a51297e0d439d1615592a34f443499a68e7e6
luba150278 <luba150278@gmail.com>
2023-01-09 16:31:30 +0200

    all

--- widget.js
+++ widget.js
@@ -1,13 +1,12 @@
 class WidgetMaker {
   constructor(...widgets) {
-      this.widgets = widgets;
+    this.widgets = widgets;
   }
   render() {
-      this.widgets.forEach( w => w.render() ) ;
+    this.widgets.forEach(w => w.render());
   }
 }
 class Rate {
-  element;
   constructor(selector) {
     const element = document.querySelector(selector)
     if (!element) {
@@ -30,7 +29,6 @@ class Rate {
 }
 
 class Moon {
-  element;
   constructor(selector) {
     const element = document.querySelector(selector)
     if (!element) {
@@ -39,20 +37,41 @@ class Moon {
     this.element = element
   }
   render() {
-      const date  = new Date() ;
-      const year  = date.getFullYear() ;
-      const month = date.getMonth() + 1 ;
-      const day   = date.getDate() ;
-      fetch(`https://www.icalendar37.net/lunar/api/?year=${year}&month=${month}&shadeColor=gray&size=50&texturize=true`)
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    fetch(`https://www.icalendar37.net/lunar/api/?year=${year}&month=${month}&shadeColor=gray&size=50&texturize=true`)
       .then(r => r.json())
       .then(j => {
-        
-        const part = Math.round( j.phase[day].lighting * 100 ) / 100 ;  
-            
+
+        const part = Math.round(j.phase[day].lighting * 100) / 100;
+
         const div = document.createElement('div');
         div.className = "widget widget-weather";
         div.innerHTML = `<div>${j.phase[day].svg}</div><b>${part} %</b>`
         this.element.appendChild(div);
       })
+  }
+}
+
+class RateBE extends Rate {
+  constructor(selector) {
+    super(selector)
+  }
+
+  render() {
+    super.render();
+    fetch('https://api.exchangerate.host/latest?base=EUR&amp;symbols=USD')
+      .then(r => r.json())
+      .then(jE => {
+        const rateE = Math.round(jE.rates.USD * 100) / 100;
+        const div = document.createElement('DIV');
+        div.style.float = "right";
+        div.style.padding = "0 4px";
+        div.style.textAlign = "center";
+        div.innerHTML = `<img src='euro.png' class='widget-img'/><br/><b>$${rateE}</b>`;
+        this.element.firstChild.insertBefore(div, this.element.firstChild.firstChild);
+      });
   }
 }
