740cf555c7913755091e83ba0c037b832434d8e4
luba150278 <luba150278@gmail.com>
2023-01-09 16:00:22 +0200

    all

--- widget.js
+++ widget.js
@@ -8,6 +8,15 @@ class Rate {
     this.element = element
   }
   render() {
-    
+    fetch('https://api.exchangerate.host/latest?base=BTC&amp;symbols=USD')
+      .then(r => r.json())
+      .then(j => {
+        const rate = Math.round(j.rates.USD);
+        const div = document.createElement('div');
+        div.className = "widget";
+        div.innerHTML = `<img src='bitcoin.png' class='widget-img'/><br/>
+        <b>$ ${rate}</b>`
+        this.element.appendChild(div);
+      })
   }
 }
